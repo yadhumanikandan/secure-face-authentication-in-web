587f9001a0d0c0a8a03a6b7ef16a641274a02630
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-04-10 22:14:03 +0530

    dataset creation

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import base64
 import os
 from os import listdir, curdir
 from os.path import isfile, join
+from PIL import Image
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "lasdfoh389h9qhweohpqe8hgqh9hqwh49hq9hgq9h"
@@ -36,6 +38,46 @@ print("Dataset Model Training Complete!!!!!")
 
 
 face_classifier = cv2.CascadeClassifier('D:\\Code\\project\\haz\\env\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+
+
+def face_extractor_dataset_creation(img):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+
+    if len(faces) == 0:
+        return None
+
+    for(x,y,w,h) in faces:
+        cropped_face = img[y:y+h, x:x+w]
+
+    return cropped_face
+
+def create_dataset_files(time):
+
+    
+    img = cv2.imread('data/received_image.jpg')
+
+    if face_extractor_dataset_creation(img) is not None:
+    # count+=1
+        face = cv2.resize(face_extractor_dataset_creation(img),(200,200))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        print(str(time))
+        # file_name_path = 'D:/DATAS/'+str(count)+'.jpg'
+        file_name_path = 'D:/DATAS/'+str(time)+'.jpg'
+
+        cv2.imwrite(file_name_path,face)
+
+        cv2.putText(face,str(time),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+        cv2.imshow('Face Cropper',face)
+        return True
+    else:
+        print("Face not found")
+        return False
+
+    # if cv2.waitKey(1)==13 or count==100:
+    #     break
+
 
 def face_detector(img, size = 0.5):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -115,6 +157,40 @@ def home():
         return render_template("home.html")
     else:
         return redirect(url_for("check_user"))
+    
+
+@app.route("/crtdata")
+def crtdata():
+    return render_template("cam_data_create.html")
+    
+
+@app.route("/create_dataset", methods=['POST'])
+def create_dataset():
+    img_data = request.form['image']
+    img_bytes = base64.b64decode(img_data.split(',')[1])
+    nparr = np.frombuffer(img_bytes, np.uint8) 
+ 
+    # Decode image
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    current_time = datetime.now()
+    current_time_str = current_time.strftime('%Y-%m-%d_%H:%M:%S')
+
+    # image = face_extractor_dataset_creation(image)
+    # image, face = face_detector(image)
+    # image = face_extractor_dataset_creation(image)
+    img_path = os.path.join(curdir, 'data/received_image.jpg')
+    cv2.imwrite(img_path, image)
+
+    bool1 = create_dataset_files(current_time_str)
+    # bool1 = True
+
+    if bool1:
+        return jsonify({'result': 'success'})
+    else:
+        return jsonify({'result': 'failure'})
+
+
 
 @app.route('/detect', methods=['POST'])
 def detect():
