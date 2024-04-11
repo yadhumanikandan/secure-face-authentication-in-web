58c0de56e61fe46c8e760514fa064b0e40b2f4b6
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-04-11 17:31:53 +0530

    single user auth ready

--- app/views.py
+++ app/views.py
@@ -23,18 +23,59 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        print(username)
-        return "logged in"
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                session['username'] = username
+                # flash("Logged in!", category="success")
+                return redirect(url_for("views.cam_login"))
+            else:
+                flash("password incorrect", category="danger")
+                return redirect(url_for("views.login"))
+        else:
+            flash("user don\'t exists", category="danger")
+            return redirect(url_for("views.login"))
     else:
         if "faceauth" in session and session["faceauth"] == True:
-            return render_template("home.html")
+            return redirect(url_for('views.home'))
         else:
             return render_template("login.html")
     
 
-@views.route("/signup")
+@views.route("/signup", methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        user_exist = User.query.filter_by(username=username).first()
+
+        if user_exist:
+            flash("Username already in use!!", category="danger")
+            return redirect(url_for("views.signup"))
+        elif password1 != password2:
+            flash("Password don\'t match", category="danger")
+            return redirect(url_for("views.signup"))
+        else:
+            # try:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1)) 
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['username'] = username
+
+            flash("user created", category="success")
+            return redirect(url_for("views.cam_register"))
+            # except:
+            #     flash("something went wrong! please try again.", category="danger")
+            #     return redirect(url_for("auth.signup"))
+    else:
+        return render_template("signup.html")
 
 
 @views.route('/cam-login')
@@ -44,9 +85,9 @@ def cam_login():
 @views.route('/home')
 def home():
     if "faceauth" in session and session["faceauth"] == True:
-        return render_template("home.html")
+        return render_template("home.html", username = session["username"])
     else:
-        return redirect(url_for("views.cam_login"))
+        return redirect(url_for("views.login"))
     
 
 @views.route("/cam-register")
@@ -89,7 +130,7 @@ def create_dataset():
 @views.route('/detect', methods=['POST'])
 def detect():
     
-    train_model()
+    # train_model()
 
     img_data = request.form['image']
     img_bytes = base64.b64decode(img_data.split(',')[1])
@@ -99,20 +140,29 @@ def detect():
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     detected = detect_img(image)
 
-    if detected==True:
+    if detected == True:
         session['faceauth'] = True
         print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        flash('Logged in successfully', category="success")
+        # return redirect(url_for('views.home'))
         return jsonify({'result': 'success'})
-        # return redirect(url_for("home"))
-    if detected==False:
-        session["faceauth"] = False
-        print("---------------------------------------------------------------------------------------------------------")
+    elif detected == False:
+        return jsonify({'result': 'failure'})
+    else:
+        print("-----------------------------------------------------------------------------------------------------------------------------------------------------")
         return jsonify({'result': 'failure'})
 
 
 @views.route('/registertologin')
 def register_to_login():
+    train_model()
     flash('Face registerd succesfully!! Now you can login to your account', category="success")
+    return redirect(url_for('views.login'))
+
+
+@views.route('/loginunsuccess')
+def login_unsuccess():
+    flash('Face not detected!!!', category="danger")
     return redirect(url_for('views.login'))
 
 
